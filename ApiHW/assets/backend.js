beab28c696e5fbfbcfe7fdb83189efa011c5bd36
morgan crosta <morgancrosta@gmail.com>
2018-03-01 16:55:38 -0800

    working on gif animate/still switch

--- ApiHW/assets/backend.js
+++ ApiHW/assets/backend.js
@@ -1,34 +1,35 @@
 //User will search for a certain type of gif
-var giphys = ["Airplanes", "Avatar the Last Airbender", "Drake", "Michael Jordan", "Beer", "Dwight Schrute", "Scooby Doo"];
+var giphys = ["Spongebob", "Avatar the Last Airbender", "Drake", "Michael Jordan", "Pizza", "Dwight Schrute", "Scooby Doo"];
 
 //keyword searched for will produce a button, with a corresponding data-attr
 function createGiphyButtons() {
 
-	$("#giphyList").empty();
+    $("#giphyList").empty();
 
 
-	for (var giphyIndex = 0; giphyIndex < giphys.length; giphyIndex++) {
+    for (var giphyIndex = 0; giphyIndex < giphys.length; giphyIndex++) {
 
-		var buttons = $("<button>");
+        
+        var buttons = $("<button>");
 
-		buttons.addClass("buttons");
-		buttons.attr("dataName", giphys[giphyIndex]);
-		buttons.text(giphys[giphyIndex]);
+        buttons.addClass("buttons");
+        buttons.attr("dataName", giphys[giphyIndex]);
+        buttons.text(giphys[giphyIndex]);
 
-		$("#giphyList").prepend(buttons);
+        $("#giphyList").prepend(buttons);
 
-	console.log(giphys);
-	}
+        console.log(giphys);
+    }
 }
 
 $("#submitBtn").on('click', function(event) {
-	event.preventDefault();
+    event.preventDefault();
 
-	var addedGiphy = $("#searchGiphys").val().trim();
+    var addedGiphy = $("#searchGiphys").val().trim();
 
-	giphys.push(addedGiphy);
+    giphys.push(addedGiphy);
 
-	createGiphyButtons();
+    createGiphyButtons();
 
 });
 
@@ -39,40 +40,56 @@ createGiphyButtons();
 //when gif is clicked on, it animates. when clicked again, it pauses
 
 function getGiphys(whatever) {
-var search = whatever.target.innerHTML;
+    var search = whatever.target.innerHTML;
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ISuos4oHv9pcxKjOyYMqefNSwXypQrIK&q=" + search + "&limit=10&offset=0&rating=G&lang=en";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ISuos4oHv9pcxKjOyYMqefNSwXypQrIK&q=" + search + "&limit=10&offset=0&rating=G&lang=en";
 
-console.log(whatever.target.innerHTML);
-
-
-
-$.ajax ({
-	url: queryURL,
-	method: 'GET',
-}).then(function(result) {
-	console.log(result);
-
-	var results = result.data;
-
-	for (var gifResults = 0; gifResults < results.length; gifResults++) {
-
-		console.log(results[gifResults]);
-
-		var gifStill = results[gifResults].images["480w_still"].url;
-		var gifAnimate = results[gifResults].images.downsized.url;
-
-		$("#gifBox").append("<img src='" + gifAnimate + "'>");
-	}
+    console.log(whatever.target.innerHTML);
 
 
+    var gifStill;
+    var gifAnimate;
 
-})
+    $.ajax({
+        url: queryURL,
+        method: 'GET',
+    }).then(function(result) {
+        console.log(result);
 
-// $("#gifBox").prepend(results.response);
+        var results = result.data;
+
+        for (var gifResults = 0; gifResults < results.length; gifResults++) {
+
+            console.log(results[gifResults]);
+
+            var gifStill = results[gifResults].images["480w_still"].url;
+            var gifAnimate = results[gifResults].images.downsized.url;
+
+
+            $("#gifBox").prepend("<img src='" + gifAnimate + "'>");
+        }
+
+
+    });
+
 }
 
+function gifAnimation() {
 
 
+
+    $("body").on('click', "img", function(event) {
+
+        console.log("you clicked a gif");
+
+        // if ()
+        //     var temp = gifStill;
+        // gifStill = gifAnimate;
+        // gifAnimate = temp;
+
+    });
+}
+
+gifAnimation();
 
 $("body").on('click', ".buttons", getGiphys);
